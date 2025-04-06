37eccb832f1085edfc28c3cdfe39c56b299e0d0d
bbrockbrown <brockbrown46@gmail.com>
2025-04-06 11:55:00 -0500

    addded home page

--- src/pages/Marketplace.jsx
+++ src/pages/Marketplace.jsx
@@ -175,12 +175,12 @@ const Marketplace = () => {
           ) : (
             items.map(item => (
               <div className="product-card" key={item.id}>
-                <div className="product-image" onClick={() => openProductModal(item)}>
+                <div className="product-image">
                   {/* Placeholder for product image */}
                   <div className="image-placeholder"></div>
                 </div>
                 <div className="product-info">
-                  <h3 onClick={() => openProductModal(item)}>{item.name}</h3>
+                  <h3 className='cursor-pointer' onClick={ () => openProductModal(item) }>{item.name}</h3>
                   <p className="product-price">${(item.price).toFixed(2)}</p>
                   {item.farmer_id && farmers[item.farmer_id] && (
                     <p className="product-farmer">
